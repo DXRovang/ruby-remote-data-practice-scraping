3f53caa27605eac917667e44813609e5447deaaa
Alexander Rovang <wolf_vii@yahoo.com>
2020-12-27 15:29:10 -0500

    commit tree 9621b8590c25c63eda5b92d1e39295e648810f56

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,10 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+
+html = open("https://flatironschool.com/") #uses the open-uri gem
+
+doc = Nokogiri::HTML(html) # condense 4 & 6
+
+puts doc.css(".site-header__hero__inner").text.strip
+
